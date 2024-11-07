4dec1950af48d051b20813e623160d308a92923b
Matt Rogers <matt.rogers@snyk.io>
2024-11-07 16:58:27 +0000

    fix: ci and release config

---
+++ release.config.mjs
@@ -0,0 +1,43 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: [
+    {
+      name: "main",
+      prerelease: false,
+    },
+    {
+      name: "rc",
+      prerelease: true,
+    },
+  ],
+  repositoryUrl: "git@github.com:snyk/snyk-universal-broker-helm.git",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/exec",
+      {
+        prepareCmd:
+          "helm dep up && helm package --version ${nextRelease.version} .",
+        publishCmd:
+          "helm push snyk-universal-broker-*.tgz oci://registry-1.docker.io/snyk",
+        execCwd: "snyk-universal-broker",
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "snyk-universal-broker/snyk-universal-broker-*.tgz",
+          },
+        ],
+        successCommentCondition: false,
+        failCommentCondition: false,
+      },
+    ],
+  ],
+};
